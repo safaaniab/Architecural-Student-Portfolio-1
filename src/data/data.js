3437eb3a41ff081e6ef77ed3f3a1ba2916b14e4d
Amani <amanisafaabhatti@gmail.com>
2025-01-16 13:15:45 -0700

    Updated portfolio

--- src/data/data.js
+++ src/data/data.js
@@ -1,11 +1,11 @@
 // CUSTOMIZE THIS FILE TO ADD YOUR OWN DATA
 
 const data = {
-    name: "John Doe",
+    name: "Amani-Safaa Bhatti",
     bio: "Undergraduate Architectural Student at University of Calgary passionate about building tools to make homes more affordable.",
     contact: {
-        email: "john.doe@gmail.com",
-        phone: "+12345678910",
+        email: "amanisafaa.bhatti@ucalgary.ca",
+        phone: "(587)429-5603",
         linkedin: "https://www.linkedin.com/",
         github: "https://www.github.com/",
     }
